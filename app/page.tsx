263e62ffb0c569c9f4a8ef7011367f176ef8461c
Air2air <todd@dunningmarketing.com>
2023-11-13 11:56:54 -0800

    Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
-import Image from "next/image";
+import { Flex, Card, Button } from "@tremor/react";
 import HistoricalElectionResults from "./components/HistoricalElectionResults";
 import CandidateInformation from "./components/CandidateInformation";
+
+const cardData = [
+  {
+    title: "Historical Election Results",
+    component: HistoricalElectionResults,
+    buttonText: "Click Here",
+  },
+  {
+    title: "Candidate Information",
+    component: CandidateInformation,
+    buttonText: "Click Here",
+  },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div style={{ width: "100%", height: "100%" }}>
+        <Flex className="gap-2">
+          {cardData.map((card, index) => (
+            <Card key={index} className="w-full h-full">
+              <h2>{card.title}</h2>
+              <card.component />
+              <Button>{card.buttonText}</Button>
+            </Card>
+          ))}
+        </Flex>
       </div>
-
-      <HistoricalElectionResults />
-      <CandidateInformation />
     </main>
   );
 }
